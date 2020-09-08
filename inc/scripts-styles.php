03eff3aedae4922ee8e0e0ada604f2eda0a09f5a
Jessica Goddard <hello@jessica-goddard.com>
2020-09-08 14:21:09 -0400

    Style fixes

--- inc/scripts-styles.php
+++ inc/scripts-styles.php
@@ -12,11 +12,11 @@
 add_action( 'wp_enqueue_scripts', 'starter_theme_scripts_styles' );
 function starter_theme_scripts_styles() {
 
-  wp_enqueue_style( 'starter-fonts', $starter_theme_config[ 'fonts-url' ] );
+  wp_enqueue_style( 'starter-theme-fonts', $starter_theme_config[ 'fonts-url' ] );
   wp_enqueue_style( 'dashicons' );
-  wp_enqueue_style( 'starter-main-css', get_stylesheet_directory_uri() . '/assets/dist/main.min.css' );
-  wp_enqueue_style( 'starter-inline-css', get_stylesheet_directory_uri() . '/assets/dist/inline.min.css' );
-  wp_enqueue_script( 'starter-main-js', get_stylesheet_directory_uri() . '/assets/dist/main.min.js', null, null, true );
+  wp_enqueue_style( 'starter-theme-main-css', get_stylesheet_directory_uri() . '/assets/dist/main.min.css' );
+  wp_enqueue_style( 'starter-theme-inline-css', get_stylesheet_directory_uri() . '/assets/dist/inline.min.css' );
+  wp_enqueue_script( 'starter-theme-main-js', get_stylesheet_directory_uri() . '/assets/dist/main.min.js', null, null, true );
 
 }
 
@@ -24,10 +24,10 @@ function starter_theme_scripts_styles() {
 add_action( 'enqueue_block_editor_assets', 'starter_theme_gutenberg_scripts_styles' );
 function starter_theme_gutenberg_scripts_styles() {
 
-  wp_enqueue_style( 'starter-fonts', $starter_theme_config[ 'fonts-url' ] );
+  wp_enqueue_style( 'starter-theme-editor-fonts', $starter_theme_config[ 'fonts-url' ] );
   wp_enqueue_style( 'dashicons' );
-  wp_enqueue_style( 'starter-editor-main-css', get_stylesheet_directory_uri() . '/assets/dist/editor.min.css' );
-  wp_enqueue_style( 'starter-inline-css', get_stylesheet_directory_uri() . '/assets/dist/inline.min.css' );
-  wp_enqueue_script( 'starter-editor-main-js', get_stylesheet_directory_uri() . '/assets/dist/editor.min.js', [ 'wp-blocks', 'wp-dom' ], null, true );
+  wp_enqueue_style( 'starter-theme-editor-main-css', get_stylesheet_directory_uri() . '/assets/dist/editor.min.css' );
+  wp_enqueue_style( 'starter-theme-editor-inline-css', get_stylesheet_directory_uri() . '/assets/dist/inline.min.css' );
+  wp_enqueue_script( 'starter-theme-editor-main-js', get_stylesheet_directory_uri() . '/assets/dist/editor.min.js', [ 'wp-blocks', 'wp-dom' ], null, true );
 
 }
